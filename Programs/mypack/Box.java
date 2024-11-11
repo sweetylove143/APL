5e95a4b1d521ff1b5c54763717e9a4e880574160
sweetylove143 <viraj.codes@gmail.com>
2024-11-11 19:46:15 +0530

    Add files via upload

---
+++ Programs/mypack/Box.java
@@ -0,0 +1,12 @@
+// save as Box.java in a folder named "mypack"
+package mypack;
+
+public class Box {
+    public int length = 100;
+    public int breadth = 200;
+
+    public void display() {
+        System.out.println("Length: " + length);
+        System.out.println("Breadth: " + breadth);
+    }
+}
